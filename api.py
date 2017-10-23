0ec865db7a2d4962f65fcefb662cec1106a80ad9
Dennis Callanan <dennis@callanan.ie>
2017-10-23 17:26:14 +0100

    gerg2

--- api.py
+++ api.py
@@ -3,7 +3,20 @@ from app import app
 @app.route('/api/test')
 def test():
 
-    return "api test"
+    print("Calling test api")
+
+    dbString = ""
+
+    with open("database.txt") as f:
+        content = f.readlines()
+        content = [x.strip() for x in content]
+
+
+        dbString = "<br>".join(content)
+
+
+        
+    return dbString
 
 
 
