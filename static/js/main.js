e89f56c9e709816abdf9570f1e8e3e6a45279c33
Dennis Callanan <dennis@callanan.ie>
2017-10-23 19:31:14 +0100

    Everything works

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,31 @@
 
+var id = 1;
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
 
 
 function setButtonClick(elem){
     console.log(this);
-    elem.style.opacity="1.0";
+    if(elem.style.opacity == "1"){
+        elem.style.opacity = "0.5";
+    }else{
+        elem.style.opacity="1.0";
+    }
+}
+
+function clickedFace(){
+
+    console.log("clicking face");
+
+    var newid =  parseInt(getRandomArbitrary(1,4));
+    while(newid == id)
+        newid =  parseInt(getRandomArbitrary(1,4));
+    id = newid;
+
+    document.getElementById("imgdesk").style="height:300px;background: url('../static/images/office"+newid+".jpg') center / cover;";
+
+    return true;
+
 }
